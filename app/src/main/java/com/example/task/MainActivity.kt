0bdafd6121a085b43f04986301fef3e9535a1b74
2blue <lpm0831@naver.com>
2022-07-23 22:38:02 +0900

    MainFirst

--- app/src/main/java/com/example/task/MainActivity.kt
+++ app/src/main/java/com/example/task/MainActivity.kt
@@ -22,17 +22,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         val a = pureumLib(5)
-
     }
 
     fun pureumLib(max: Int){
         val regex = Regex("#[\\w]{1,${max}}")
-        val result = ""
-        var startPoint = 0
-
-
         binding.myEdit.addTextChangedListener(object :TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun afterTextChanged(s: Editable?) {}
@@ -73,93 +67,7 @@ class MainActivity : AppCompatActivity() {
                 else{
                     println("no")
                 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//                    println("inList")
-////                    Count = 1
-////                    val myListSize = matchResult.toList().size
-//                    for(i in matchResult.toList()){
-//                        if(Count > max+1) {
-//                            Count = 1
-//                            startPoint += Count
-//                        }
-//                        val myGap = i.value
-//                        val myLoation = binding.myEdit.text.indexOf(myGap,startPoint)
-//
-//                        try {
-//                            binding.myEdit.text.setSpan(
-//                                ForegroundColorSpan(Color.RED),
-//                                myLoation,
-//                                myLoation + Count,
-//                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-//                            )
-//                        }catch (e:Exception) {println("E")}
-//                        Count+=1
-//                    }
-//                    binding.myEdit.setTextColor(Color.BLUE)
-
-
-
-
-
-
-//                    if(Count == max+2){
-//                        startPoint = binding.myEdit.text.length
-//                        println("startPoint : $startPoint")
-//                        Count = 1
-//                    }
-//
-//                    val nowLocation = binding.myEdit.text.indexOf("#", startPoint)
-//
-//                    println("nowLocation : $nowLocation")
-//                    println("Count : $Count")
-//
-//                    try {
-//                        binding.myEdit.text?.setSpan(
-//                            ForegroundColorSpan(Color.RED),
-//                            nowLocation,
-//                            nowLocation + Count,
-//                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-//                        )
-//                        Count += 1
-//                        startPoint
-//                    }catch (e :Exception){println(e)}
-
-//                matchResult.forEach {
-//                    println("match value: ${it.value}")
-
-
-//                    if (Count <= max){
-//                        println("match value: ${it.value}")
-//                        val startIndex = binding.myEdit.text.indexOf(it.value)
-//                        println("startIndex : $startIndex")
-//                        println("Count : $Count")
-//                        try {
-//                            binding.myEdit.text?.setSpan(
-//                                ForegroundColorSpan(Color.RED),
-//                                startIndex,
-//                                startIndex+Count+1,
-//                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-//                            )
-//                        }catch (e : Exception){
-//                            println("$e")
-//                        }
-//                        Count+=1
-//                    }else Count=0
-//                }
             }
-
         })
     }
 }
