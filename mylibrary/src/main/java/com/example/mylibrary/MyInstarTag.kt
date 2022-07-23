0aefb6593d5cfa84a2a84cf8ea2c0d90075ec7f3
2blue <lpm0831@naver.com>
2022-07-23 23:16:55 +0900

    Finall Lib

--- mylibrary/src/main/java/com/example/mylibrary/MyInstarTag.kt
+++ mylibrary/src/main/java/com/example/mylibrary/MyInstarTag.kt
@@ -2,17 +2,13 @@ package com.example.mylibrary
 
 import android.content.Context
 import android.graphics.Color
-import android.os.Build
 import android.text.Editable
 import android.text.Spannable
 import android.text.TextWatcher
 import android.text.style.ForegroundColorSpan
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.EditText
-import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.res.ResourcesCompat.getColor
 import com.example.mylibrary.databinding.MyinstartagBinding
 import java.lang.Exception
 
@@ -21,43 +17,48 @@ class MyInstarTag(context: Context, attrs: AttributeSet) : ConstraintLayout(cont
         MyinstartagBinding.inflate(LayoutInflater.from(context),this,true)
     }
 
-    fun pureumLib(max: Int) {
+    fun pureumLib(max: Int){
+        var Count = 1
+        var nowGap = ""
+        val regex = Regex("#[\\w]{1,${max}}")
 
-
-        var Count = 0
-        var myStart = 0
-
-
-        binding.myEdit.addTextChangedListener(object : TextWatcher {
+        binding.myEdit.addTextChangedListener(object :TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun afterTextChanged(s: Editable?) {}
-
-            @RequiresApi(Build.VERSION_CODES.M)
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (binding.myEdit.text.length == 0) {
-                    Count = 0
-                    myStart = 0
-                }
-                val myIndex = binding.myEdit.text.toString().indexOf("#", myStart)
 
-                if (myIndex != -1 && Count <= max + 1) {
-                    try {
-                        binding.myEdit.text?.setSpan(
-                            ForegroundColorSpan(Color.RED),
-                            myIndex,
-                            myIndex + Count,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
-                    } catch (e: Exception) {
-                        println("$e")
+                val myText = binding.myEdit.text
+                val matchResult:Sequence<MatchResult> = regex.findAll(myText)
+
+                myText.setSpan(
+                    ForegroundColorSpan(Color.BLACK),
+                    0,
+                    myText.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+
+                if(matchResult.toList().isNotEmpty()) {
+                    Count = 0
+                    for (i in matchResult.toList()) {
+                        nowGap = i.value
+                        val myStartLoation = myText.indexOf(i.value, Count)
+                        try {
+                            myText.setSpan(
+                                ForegroundColorSpan(Color.RED),
+                                myStartLoation,
+                                myStartLoation + i.value.length,
+                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                            )
+                            Count = myStartLoation + i.value.length
+                            println("after Count : ${Count}")
+
+                        }catch (e:Exception){
+                            println("e : $e")
+                        }
                     }
-                    Count += 1
                 }
-
-                if (Count > max + 1) {
-                    println("check!!")
-                    Count = 0
-                    myStart = binding.myEdit.text.length
+                else{
+                    println("no")
                 }
             }
         })
